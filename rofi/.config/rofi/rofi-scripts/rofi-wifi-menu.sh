bd8ec137887c3b50b49998844dd92603b9d11813
cheewei <sohcheewei@u.nus.edu>
2023-10-14 22:50:55 +0800

    added notifications for wifi menu

--- rofi/.config/rofi/rofi-scripts/rofi-wifi-menu.sh
+++ rofi/.config/rofi/rofi-scripts/rofi-wifi-menu.sh
@@ -6,9 +6,9 @@ wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  
 
 connected=$(nmcli -fields WIFI g)
 if [[ "$connected" =~ "enabled" ]]; then
-	toggle="󰖪  Disable Wi-Fi"
+	toggle="󰖪  Disable Wifi"
 elif [[ "$connected" =~ "disabled" ]]; then
-	toggle="󰖩  Enable Wi-Fi"
+	toggle="󰖩  Enable Wifi"
 fi
 
 # Use rofi to select wifi network
@@ -18,21 +18,21 @@ chosen_id=$(echo "${chosen_network:3}" | xargs)
 
 if [ "$chosen_network" = "" ]; then
 	exit
-elif [ "$chosen_network" = "󰖩  Enable Wi-Fi" ]; then
-	nmcli radio wifi on
-elif [ "$chosen_network" = "󰖪  Disable Wi-Fi" ]; then
-	nmcli radio wifi off
+elif [ "$chosen_network" = "󰖩  Enable Wifi" ]; then
+	nmcli radio wifi on && notify-send "Network Connection" "Wifi Enabled"
+elif [ "$chosen_network" = "󰖪  Disable Wifi" ]; then
+	nmcli radio wifi off && notify-send "Network Connection" "Wifi Disabled"
 else
 	# Message to show when connection is activated successfully
-	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
+	success_message="You are now connected to the Wifi network \"$chosen_id\"."
 	# Get saved connections
 	saved_connections=$(nmcli -g NAME connection)
 	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
-		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
+		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Network Connection" "$success_message"
 	else
 		if [[ "$chosen_network" =~ "" ]]; then
 			wifi_password=$(rofi -dmenu -p "Password: " )
 		fi
-		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
+		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Network Connection" "$success_message"
 	fi
 fi
