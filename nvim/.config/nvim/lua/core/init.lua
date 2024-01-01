4806ba3f614f8f68a33f35090fc3e7a09481585e
cheewei <sohcheewei@u.nus.edu>
2024-01-01 21:36:17 +0800

    work in progress

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,18 +15,17 @@ vim.opt.rtp:prepend(lazypath)
 require("core.globals")
 require("core.options")
 require("core.keymaps")
-require("core.autocmds")
 
 local opts = {
-	defaults = {
-		lazy = true,
-	},
-	rtp = {
-		disabled_plugins = {
-			"netrw",
-			"netrwPlugin",
-		}
-	},
+    defaults = {
+        lazy = true,
+    },
+    rtp = {
+        disabled_plugins = {
+            "netrw",
+            "netrwPlugin",
+        }
+    },
 }
 
 local plugins = "plugins"
--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,122 +1,39 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	dependencies = {
-		"williamboman/mason.nvim",
-		"windwp/nvim-autopairs",
-		"creativenull/efmls-configs-nvim",
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local diagnostic_signs = require("util.lsp").diagnostic_signs
-		local on_attach = require("util.lsp").on_attach
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+        auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		for type, icon in pairs(diagnostic_signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
-		-- Lua
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = {
-							[vim.fn.expand("VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-					},
-				},
-			},
-		})
-
-		-- Python
-		lspconfig.pylsp.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				pylsp = {
-					plugins = {
-						black = {
-							enabled = true,
-							line_length = 1000,
-						},
-						flake8 = {
-							enable = true,
-							maxLineLength = 1000,
-						},
-						pycodestyle = {
-							enabled = true,
-							maxLineLength = 1000,
-							ignore = {
-								"E203", -- whitespace before ",", ";", or ":"
-								"E501", -- line too long
-								"W293", -- blank line contains whitespace
-								"W391", -- blank line at end of file
-								-- "E226", -- missing whitespace around arithmetic operator
-							},
-						},
-					},
-				},
-			},
-		})
-
-		-- Java
-		lspconfig.jdtls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-
-		-- local luacheck = require("efmls-configs.linters.luacheck")
-		local stylua = require("efmls-configs.formatters.stylua")
-		-- local flake8 = require("efml-configs.linters.flake8")
-		local black = require("efmls-configs.formatters.black")
-
-		local java_format_config = {
-			filetypes = { "java" },
-			init_options = {
-				filetypes = { "java" },
-				format = {
-					command = "google-java-format",
-					args = {
-						"--style=google",
-						"--length=120",
-						"--indent_spaces=4",
-						"--align_type_annotations",
-					},
-				},
-			},
-		}
-
-		lspconfig.efm.setup({
-			filetypes = {
-				"lua",
-				"python",
-				"java",
-			},
-			init_options = {
-				documentFormatting = true,
-				documentRangeFormatting = true,
-				hover = true,
-				documentSymbol = true,
-				codeAction = true,
-				completion = true,
-			},
-			settings = {
-				languages = {
-					lua = { stylua },
-					python = { black },
-					java = { java_format_config },
-				},
-			},
-		})
-	end,
+            local lspconfig = require("lspconfig")
+            lspconfig.pylsp.setup({
+                capabilites = capabilities,
+            })
+            lspconfig.lua_ls.setup({
+                capabilites = capabilities,
+            })
+            
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
